dcd7ec7d996117da18b055f27de6572b284972ec
Volkan Özçelik <ovolkan@vmware.com>
2024-03-09 03:57:56 -0800

    dashboard

---
+++ docs/k8s101/lecture001/src/lab-000/hack/dashboard.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# ( Geyik Academy )
+#   `--(_   _)--'
+#        Y-Y
+#       /@@ \  Si audes scire,
+#      /     \    cervam sapientiae insequere.
+#      `--'.  \             ,
+#          |   `.__________/)
+
+minikube dashboard
